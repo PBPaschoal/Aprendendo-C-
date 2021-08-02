f1d5e216951f176d30d699815ec40646d29e879c
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2021-08-01 22:24:14 -0300

    Aula 36
    Rio de Janeiro Dia 01-08-2021
    Aula 36 Sintaxe Opcional: operadores de atribuição cumulativa
    Problema exemplo:
    Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de telefone. Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00. Fazer um programa para ler a quantidade de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.
    Exemplo:
    Entrada:                                      Saída:
    22                                               Valor a pagar: R$ 50.00
    Entrada:                                      Saída:
    103                                             Valor a pagar: R$ 56.00

--- Aula 36/Aula 36/Program.cs
+++ Aula 36/Aula 36/Program.cs
@@ -1,44 +1,36 @@
-﻿//Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de telefone. 
-//Cada minuto que exceder a franquia de 100 minutos custa R$ 2.00. Fazer um programa para ler a quantidade 
-//de minutos que uma pessoa consumiu, daí mostrar o valor a ser pago.
+﻿//Rio de Janeiro Dia 01-08-2021
+//Aula 36 Sintaxe Opcional: operadores de atribuição cumulativa
+//Problema exemplo:
+//Uma operadora de telefonia cobra R$ 50.00 por um plano básico que dá direito a 100 minutos de telefone. Cada minuto que exceder
+//a franquia de 100 minutos custa R$ 2.00. Fazer um programa para ler a quantidade de minutos que uma pessoa consumiu, daí mostrar
+//o valor a ser pago.
 //Exemplo:
-//Entrada: 22    Saída: Valor a pagar: R$ 50.00
-//Entrada: 103   Saída: Valor a pagar: R$ 56.00
+//Entrada:                                      Saída:
+//22                                            Valor a pagar: R$ 50.00
+//Entrada:                                      Saída:
+//103                                           Valor a pagar: R$ 56.00
 
 using System;
+using System.Globalization;
 
-namespace Aula_36
+namespace SintaxeOpcional_Aula_36
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Meu jeito:
-            int v1, v2;
-            v1 = int.Parse(Console.ReadLine());
+            int minutos;
+            double conta;
 
-            if (v1 <= 50) {
-                Console.WriteLine("Valor a pagar: R$ 50.00");
-            } else
+            minutos = int.Parse(Console.ReadLine());
+
+            conta = 50.0;
+            if (minutos > 100)
             {
-                v2 = (v1 - 100) * 2 + 50;
-                Console.WriteLine("Valor a pagar: R$ " + v2 + ".00");
+                conta += (minutos - 100) * 2.0;
             }
-            //OU como o professor ensinou:
-            /*
-             * int minutos;
-             * double conta;
-             * 
-             * minutos = int.Parse(Console.ReadLine());
-             * 
-             * conta = 50.0;
-             * if (minutos > 100) {
-             *     conta = conta + (minutos - 100) * 2.0;
-             * }
-             * 
-             * Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
-             * Console.ReadLine();
-             */
+
+            Console.WriteLine("Valor a pagar: R$ " + conta.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
